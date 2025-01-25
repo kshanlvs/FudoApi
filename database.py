f6e4608d45680706b34c4d9dc4b4d8e5a1e37497
kishan kumar <sbittu348@gmail.com>
2025-01-25 18:59:52 +0530

    user post api created

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:xzSbFFihYkYGaLkWfbHEUfckPqFeqmAp@postgres.railway.internal:5432/railway"
+DATABASE_URL = "postgresql://postgres:ciJYamMAqdFQUNyIwLysHqUCSrNiHzDJ@viaduct.proxy.rlwy.net:19536/railway"
 
 # Create the database engine
 engine = create_engine(DATABASE_URL)
--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 
+from fastapi import FastAPI, HTTPException, Depends
 
+from database import get_db
+from models import User
 
 from fastapi import  FastAPI
+from sqlalchemy.orm import Session
+
+from schemas import UserCreate
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.post("/users/", response_model=dict)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"id": new_user.id, "name": new_user.name, "email": new_user.email}
