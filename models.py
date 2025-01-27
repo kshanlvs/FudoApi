f0cbd624d6211491b88595d5373286279a3588b2
kishan kumar <sbittu348@gmail.com>
2025-01-27 19:42:44 +0530

    user table modified

--- models.py
+++ models.py
@@ -7,3 +7,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
+    phone = Column(String, unique=True, index=True)  # Added phone field
+    password = Column(String)
