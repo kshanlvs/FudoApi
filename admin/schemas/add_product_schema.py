3cce7d77c2f43e6680861aa9eb4241fde91de213
kishan kumar <sbittu348@gmail.com>
2025-02-10 15:33:19 +0530

    added cat id to product api

--- admin/schemas/add_product_schema.py
+++ admin/schemas/add_product_schema.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
+
 class ProductCreate(BaseModel):
     name: str
     description: str | None = None
     price: float  # Required field for product price
     image_url: str | None = None
+    category_id: int 
